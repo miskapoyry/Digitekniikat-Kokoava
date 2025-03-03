ce66cc7b0cd88b7886e2f9ec90e303aa532c1afd
Miska Poyry <bhd692@myy.haaga-helia.fi>
2025-03-03 15:43:47 +0200

    Tee alustava listaus elokuvista kayttamalla cards

--- js/customScript.js
+++ js/customScript.js
@@ -1,20 +1,23 @@
+let movieCount = 3;
+let data = []
+
 async function fetchMovies() {
     try{
         const response = await fetch("https://ghibliapi.vercel.app/films");
-        const data = await response.json();
-        showMovies(data);
+        data = await response.json();
+        showMovies();
     } catch(error) {
         document.getElementById("movie-listing").innerHTML = "<p>Couldn't fetch movie data </p>" + error;
     }
 }
 
-function showMovies(data){
+function showMovies(){
     let text = document.getElementById("movie-listing");
     text.innerHTML = "";
 
     console.log(data)
 
-    data.forEach(movie => {
+    data.slice(0,movieCount).forEach(movie => {
         let card = `
             <div class="mt-3 mb-3 col-lg-4 col-md-4">
                 <div class="card h-100 shadow">
@@ -22,13 +25,24 @@ function showMovies(data){
                     <div class="card-body d-flex flex-column text-center">
                         <h5 class="card-title text-center">${movie.title}</h5>
                         <p class="card-text">${movie.description.substring(0,80)}...</p>
-                        <a href="#" class="btn btn-primary mt-auto align-self-center">Go somewhere</a>
+                        <a href="#" class="btn btn-primary mt-auto align-self-center">More info</a>
                     </div>
                 </div>
             </div>
         `
         text.innerHTML += card;
     });
+
+    // MIKÄLI MOVIE COUNT ON YLI MOVIE LISTAN PITUUDEN, LAITETAAN MOREMOVIES NAPIN DISPLAY TILAAN NONE, JOLLOIN SITÄ EI NÄYTETÄ
+
+    if(movieCount >= data.length){
+        document.getElementById("moreMovies").style.display = "none";
+    }
 }
+
+document.getElementById("moreMovies").addEventListener("click", function(){
+    movieCount += 3;
+    showMovies();
+})
 
 document.addEventListener("DOMContentLoaded", fetchMovies);
