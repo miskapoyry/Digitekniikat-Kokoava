4cf91cd7544d8f71bfa85056af2f89f0d7bdde58
Miska Poyry <bhd692@myy.haaga-helia.fi>
2025-03-03 17:18:38 +0200

    Tee pienia muutoksia ulkoasuun

--- js/customScript.js
+++ js/customScript.js
@@ -25,7 +25,7 @@ function showMovies(){
                     <div class="card-body d-flex flex-column text-center">
                         <h5 class="card-title text-center">${movie.title}</h5>
                         <p class="card-text">${movie.description.substring(0,80)}...</p>
-                        <a href="#" class="btn btn-primary mt-auto align-self-center">More info</a>
+                        <a href="#" class="btn btn-primary mt-auto align-self-center"> Discover </a>
                     </div>
                 </div>
             </div>
@@ -36,13 +36,32 @@ function showMovies(){
     // MIKÄLI MOVIE COUNT ON YLI MOVIE LISTAN PITUUDEN, LAITETAAN MOREMOVIES NAPIN DISPLAY TILAAN NONE, JOLLOIN SITÄ EI NÄYTETÄ
 
     if(movieCount >= data.length){
-        document.getElementById("moreMovies").style.display = "none";
+        document.getElementById("moreMovies").classList.add("d-none");
+        document.getElementById("lessMovies").classList.remove("d-none");
     }
 }
 
 document.getElementById("moreMovies").addEventListener("click", function(){
     movieCount += 3;
     showMovies();
+})
+
+// Nämä kaikki toimet tehdään, kun käyttäjä klikkaa buttonia jonka id on "lessMovies"
+document.getElementById("lessMovies").addEventListener("click", function(){
+    // Asetetaan VAIN 3 elokuvaa näkyviin
+    movieCount = 3;
+    
+    // Kutsutaan showMovies() -funktiota
+    showMovies();
+
+    // Laitetaan Show less -button display none tilaan
+    this.classList.add("d-none");
+
+    // Laitetaan Show More -buttonista display none pois
+    document.getElementById("moreMovies").classList.remove("d-none");
+
+    // Klikkauksen jälkeen scrollataan movies kohdan alkuun, jotta voidaan näyttää vain kolme elokuvaa
+    document.getElementById("movies").scrollIntoView();
 })
 
 document.addEventListener("DOMContentLoaded", fetchMovies);
